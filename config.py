dbf6f570e3cdf39a2bfb28de9963e0db4fc22085
Error1234543 <ptaviyad21@gmail.com>
2025-08-21 15:23:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7889827043:AAH5KTrUZ0m_XQLJrZ8h1mhWgHrahwYEy1E")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "22581733"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8146069942:AAH1JDfbPx2Vz1dSuaUBoheKUdXYCnJdJkM")  # Ensure correct key name
+    API_ID = int(os.environ.get("API_ID", "20619533"))  # Added key name and default value
+    API_HASH = os.environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "6530997270").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "7447651332").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
