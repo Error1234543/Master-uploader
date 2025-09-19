f67da1f9aefb69c695553d7d90bd4c20366cd752
Error1234543 <ptaviyad21@gmail.com>
2025-09-19 10:11:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", "20619533"))  # Added key name and default value
     API_HASH = os.environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "7447651332").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "8226637107").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
